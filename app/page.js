3e08ddd554f2d27a28c8fd72e93ea9432591de67
prabhnoorLubana2004 <164228045+prabhnoorLubana2004@users.noreply.github.com>
2024-09-24 12:47:51 -0600

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <li><Link href="/week-2">Week 2</Link></li>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Prabhnoor Singh</h1>
+            <p><Link href="https://github.com/prabhnoorLubana2004">GitHub Repository</Link></p>
+        </main>
+    );
+}
