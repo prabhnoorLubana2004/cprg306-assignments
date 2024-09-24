eb015e68c2170bc7dcf42e558c170c1145f121ab
prabhnoorLubana2004 <164228045+prabhnoorLubana2004@users.noreply.github.com>
2024-09-24 12:51:29 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
     return (
         <main>
             <h1>Prabhnoor Singh</h1>
-            <p><Link href="https://github.com/prabhnoorLubana2004">GitHub Repository</Link></p>
+            <p><Link href="https://github.com/prabhnoorLubana2004/cprg306-assignments">GitHub Repository</Link></p>
         </main>
     );
 }
