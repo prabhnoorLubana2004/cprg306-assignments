b3c403704aeceb227a3975dc784117ea9ba41e8b
prabhnoorLubana2004 <164228045+prabhnoorLubana2004@users.noreply.github.com>
2024-10-01 15:24:11 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function Page() {
+export default function HomePage() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <li><Link href="/week-2">Week 2</Link></li>
+      <p>
+        <Link href="/week-2" passHref>
+          Week 2 Assignment
+        </Link>
+      </p>
+      <p>
+        <Link href="/week-3" passHref>
+          Week 3 Assignment
+        </Link>
+      </p>
     </main>
   );
 }
