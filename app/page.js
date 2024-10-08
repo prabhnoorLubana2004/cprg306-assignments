a68b8ed58a4dbd2180904f4877b9030055b2b2d3
prabhnoorLubana2004 <164228045+prabhnoorLubana2004@users.noreply.github.com>
2024-10-08 10:19:59 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import Link from 'next/link';
-
-export default function HomePage() {
+import Link from "next/link";
+export default function Page() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        <Link href="/week-2" passHref>
-          Week 2 Assignment
-        </Link>
-      </p>
-      <p>
-        <Link href="/week-3" passHref>
-          Week 3 Assignment
-        </Link>
-      </p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold mb-5">
+          CPRG 306: Web Development 2 - Assignments
+        </h1>
+        <div className="text-lg">
+          <ul>
+            <li className="hover:text-green-400 hover:underline">
+              <Link href="/week-2">Week 2 Assignment</Link>
+            </li>
+            <li className="hover:text-green-400 hover:underline">
+              <Link href="/week-3">Week 3 Assignment</Link>
+            </li>
+            <li className="hover:text-green-400 hover:underline">
+              <Link href="/week-4">Week 4 Assignment</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
