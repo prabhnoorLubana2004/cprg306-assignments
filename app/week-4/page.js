6d17900f21c6313497fbec8063aef01d6d915bc7
prabhnoorLubana2004 <164228045+prabhnoorLubana2004@users.noreply.github.com>
2024-10-08 10:51:10 -0600

    Add files via upload

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main className="flex justify-center w-full bg-slate-100">
+      <NewItem />
+    </main>
+  );
+}
+//
