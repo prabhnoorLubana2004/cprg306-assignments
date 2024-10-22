44f5c3113bc3c2edd674c4c100a416566bb7e210
prabhnoorLubana2004 <164228045+prabhnoorLubana2004@users.noreply.github.com>
2024-10-21 20:04:19 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,6 +20,9 @@ export default function Page() {
             <li className="hover:text-green-400 hover:underline">
               <Link href="/week-5">Week 5 Assignment</Link>
             </li>
+            <li className="hover:text-green-400 hover:underline">
+              <Link href="/week-6">Week 6 Assignment</Link>
+            </li>
             
             
           </ul>
